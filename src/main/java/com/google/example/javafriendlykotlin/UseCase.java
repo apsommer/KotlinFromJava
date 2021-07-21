7520d97a972bf059f6bc7897e647d01ce335333a
Drew Sommer <andrew.sommer@tiaa.org>
2021-07-21 09:55:26 -0500

    fix: Annotate package-level functions

--- src/main/java/com/google/example/javafriendlykotlin/UseCase.java
+++ src/main/java/com/google/example/javafriendlykotlin/UseCase.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class UseCase {
 
     public static User registerGuest(String name) {
-        User guest = new User(Repository.getNextGuestId(), StringUtilsKt.nameToLogin(name), name);
+        User guest = new User(Repository.getNextGuestId(), StringUtils.nameToLogin(name), name);
         Repository.addUser(guest);
         return guest;
     }
--- src/main/kotlin/com/google/example/javafriendlykotlin/StringUtils.kt
+++ src/main/kotlin/com/google/example/javafriendlykotlin/StringUtils.kt
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+@file: JvmName("StringUtils")
 package com.google.example.javafriendlykotlin
 
 fun String.toTitleCase(): String {
